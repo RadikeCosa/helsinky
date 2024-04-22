c2f5af1701e06d556d3b82d80f8c3e33796f3c83
Ramiro Cosa <ramirocosa@gmail.com>
2024-04-22 09:58:24 -0300

    que onda

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
 import { useState } from "react";
 import Note from "./components/Note";
 
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState("");
+  const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault();
+    console.log("button clicked", event.target);
     const noteObject = {
       content: newNote,
-      important: Math.random() > 0.5,
+      important: Math.random() < 0.5,
       id: notes.length + 1,
     };
 
     setNotes(notes.concat(noteObject));
     setNewNote("");
   };
-
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
@@ -27,12 +25,13 @@ const App = (props) => {
   const notesToShow = showAll
     ? notes
     : notes.filter((note) => note.important === true);
+
   return (
     <div>
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          Show {showAll ? "important" : "all"}
+          show {showAll ? "important" : "all"}
         </button>
       </div>
       <ul>
