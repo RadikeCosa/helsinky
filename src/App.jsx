0e66b0683c101801e43a5c4867cac490d405e357
Ramiro Cosa <ramirocosa@gmail.com>
2024-04-24 19:35:09 -0300

    useEffect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Contact from "./components/Contact";
 
-const App = (props) => {
-  const [contacts, setContacts] = useState(props.contacts);
+const App = () => {
+  const [contacts, setContacts] = useState([]);
   const [newContact, setNewContact] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log("promise fullfilled");
+      setContacts(response.data);
+    });
+  }, []);
+  console.log("reder", contacts.length, "notes");
 
   const addContact = (event) => {
     event.preventDefault();
@@ -14,6 +24,7 @@ const App = (props) => {
     };
     setContacts(contacts.concat(contactObject));
     setNewContact("");
+    setNewNumber("");
   };
   const handleContactChange = (event) => {
     setNewContact(event.target.value);
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,4 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-const contacts = [
-  { name: "Arto Hellas", number: "040-123456", id: 1 },
-  { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-  { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-  { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-];
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App contacts={contacts} />
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
